81dd9a0ec9edcb2061eb3238ebb24eae2b7ebcbe
programmingsteve <programmingsteve@gmail.com>
2026-07-09 03:14:02 +0900

    Adjust formatting

--- py100/reading_documentation_2/1.py
+++ py100/reading_documentation_2/1.py
@@ -1,6 +1,9 @@
 # String Formatting
 
-# Python offers multiple ways to format strings. The str.format method is a popular method, but since Python 3.6, a new way called f-strings (formatted string literals) was introduced. F-strings offer a concise way to embed expressions inside string literals.
+# Python offers multiple ways to format strings. The str.format method is a
+# popular method, but since Python 3.6, a new way called f-strings (formatted
+# string literals) was introduced. F-strings offer a concise way to embed
+# expressions inside string literals.
 
 # Given the following variables:
 
@@ -13,4 +16,5 @@ profession = "programmer"
 
 # How can you achieve the same result using an f-string?
 
-# Refer to the Python documentation on Format String Syntax and Formatted string literals for guidance.
+# Refer to the Python documentation on Format String Syntax and Formatted
+# string literals for guidance.
